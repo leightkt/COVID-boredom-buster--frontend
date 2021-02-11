a4d37745f53338fac8e3aafbf4fa032331162516
Kat Leight <leightkt@gmail.com>
2021-02-11 14:33:16 -0700

    missed a /

--- showFavorites.js
+++ showFavorites.js
@@ -31,7 +31,7 @@ let favoriteActivities = null
 
 setActivityOptions()
 
-fetch(`${backendURL}users/${userId}`)
+fetch(`${backendURL}/users/${userId}`)
     .then(response => response.json())
     .then(user =>{
         console.log(user)
